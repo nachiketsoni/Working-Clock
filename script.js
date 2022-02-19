d33cf4019ffdaf2102c493fb4665617f317fe6e6
nachiketsoni <nxchikxt@gmail.com>
2022-02-19 14:20:01 +0530

    ne commit

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var time= document.querySelector("#time");
-function showTime(){
+var time = document.querySelector("#time");
+function showTime() {
     var today = new Date()
     var hour = today.getHours()
     var min = today.getMinutes()
     var sec = today.getSeconds()
 
 
-var AMPM;
-if (hour < 12){
-    AMPM = "AM"
-}
-else{
-    AMPM = "PM"
-}
+    var AMPM;
+    if (hour < 12) {
+        AMPM = "AM"
+    }
+    else {
+        AMPM = "PM"
+    }
 
-hour = hour % 12 || 12;
+    hour = hour % 12 || 12;
 
-time.innerHTML = `<h1 id="time" ><span>${addZero(hour)}</span> : <span>${addZero(min)}</span> : <span>${addZero(sec)}</span><span>${AMPM}</span></h1>`
- function addZero (n){
-   return  (Math.floor(n, 10) <10 ? '0' : '') + n
+    time.innerHTML = `<h1 id="time" ><span>${addZero(hour)}</span> : <span>${addZero(min)}</span> : <span>${addZero(sec)}</span><span>${AMPM}</span></h1>`
+    function addZero(n) {
+        return (Math.floor(n, 10) < 10 ? '0' : '') + n
     };
-setTimeout(showTime, 1000);
+    setTimeout(showTime, 1000);
 }
 showTime();
 
